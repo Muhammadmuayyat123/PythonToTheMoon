001881d025da457b40362f314f6aaef8929c05f1
Muhammad muayyat <100189331+Muhammadmuayyat123@users.noreply.github.com>
2022-03-14 00:24:15 +0800

    Add files via upload

---
+++ TIPE DATA.py
@@ -0,0 +1,17 @@
+#TIPE DATA PADA PYTHON
+#BOOLEAN
+print(True)
+print(False)
+
+#STRING
+print('Ini adalah string')
+print("Ini Adalah String")
+
+#INTEGER
+print(25)
+print(123)
+
+#FLOAT
+print(3.13)
+
+
